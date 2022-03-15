21b8c9f59ffc174a40e39bbd75e4c86d48d23230
Reuven Lerner <reuven@lerner.co.il>
2022-03-15 12:53:33 +0200

    gitautosync update 72

--- myprog.py
+++ myprog.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 
-def hello(name):
+def hello(name: str) -> str:
     return f'Hello, {name}!'
 
 
 print(hello('world'))
-print(hello(5))
-print(hello(hello))
+# print(hello(5))
+# print(hello(hello))
+
+
+def mysum(*args):
+    total = 0
+
+    for one_arg in args:
+        total += one_arg
+
+    return total
+
+
+print(mysum(10, 20, 30))
+print(mysum(10, 'a', 30))
+print(mysum(10, 20.5, 30))
