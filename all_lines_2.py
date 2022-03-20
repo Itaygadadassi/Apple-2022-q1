0f92f97c4034dcec09a5fa13a52e47668a723399
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 11:28:27 +0200

    gitautosync update 64

--- all_lines_2.py
+++ all_lines_2.py
@@ -11,12 +11,7 @@ q = queue.Queue()
 
 def all_lines(one_filename):
     for one_line in open(one_filename):
-
-
-def all_lines(*filenames):
-    for one_filename in filenames:
-        for one_line in open(one_filename):
-            print(one_line)
+        q.put(one_line)
 
 
 start_time = time.time()
