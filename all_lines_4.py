e8eac73c6165d8a058e6abf075f965d479d08205
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 12:33:20 +0200

    gitautosync update 118

--- all_lines_4.py
+++ all_lines_4.py
@@ -13,7 +13,6 @@ def all_lines(one_filename):
 with ThreadPoolExecutor() as executor:
     results = executor.map(all_lines, glob.glob('*.py'))
 
-
 # threading: Total time = 0.0012621879577636719
 # serial:  = Total time = 0.0006771087646484375
 # executor:  Total time = 0.0044422149658203125
