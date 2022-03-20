505c1ce41ad6483159c54d227a35d9332e749e78
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 12:01:47 +0200

    gitautosync update 80

--- mythread3.py
+++ mythread3.py
@@ -8,3 +8,10 @@ import random
 def hello(n):
     time.sleep(random.randint(0, 3))
     return f'{n} Hello!'
+
+
+with ThreadPoolExecutor() as executor:
+    all_futures = []
+    for i in range(10):
+        f = executor.submit(hello, i)
+        all_futures.append(f)
