5617d13bb8b75508fee025347401cd2339ee8034
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 12:34:50 +0200

    gitautosync update 122

--- all_lines_4.py
+++ all_lines_4.py
@@ -22,3 +22,4 @@ print(f'Total time = {end_time - start_time}')
 # threading: Total time = 0.0012621879577636719
 # serial:  = Total time = 0.0006771087646484375
 # executor:  Total time = 0.0044422149658203125
+# map:       Total time = 0.001125335693359375
