47d3fc888547a0ca7104f4354ae8990297eb1edd
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 12:06:57 +0200

    gitautosync update 89

---
+++ mythread4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor, wait
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    return f'{n} Hello!'
+
+
+with ThreadPoolExecutor(max_workers=20) as executor:
+    all_futures = []
+    for i in range(10):
+        f = executor.submit(hello, i)
+        all_futures.append(f)
+
+    done, not_done = wait(all_futures)
+
+    for one_item in done:
+        print(one_item.result())
