07650922904095eee432a2a8fb067a351bf43d05
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 12:01:02 +0200

    gitautosync update 78

--- mythread3.py
+++ mythread3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env import
 
 from concurrent.futures import ThreadPoolExecutor
+import time
+import random
 
 
 def hello(n):
+    time.sleep(random.randint(0, 3))
     return f'{n} Hello!'
