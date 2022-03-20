480b089622f08bc324609cb4291a904c5a90f049
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 10:53:40 +0200

    gitautosync update 40

--- mythread1.py
+++ mythread1.py
@@ -19,5 +19,8 @@ for i in range(10):
 
 # when we get to this line, all threads should be done
 
+for one_thread in all_threads:
+    one_thread.join()           # wait for the thread to finish
+
 
 print('Done!')
