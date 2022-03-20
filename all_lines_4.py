d6a3f087eecaebf2ffabb168b4bce42e908d4781
Reuven Lerner <reuven@lerner.co.il>
2022-03-20 12:32:19 +0200

    gitautosync update 114

---
+++ all_lines_4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor, wait
+import queue
+import glob
+import time
+
+
+def all_lines(one_filename):
+    for one_line in open(one_filename):
+        q.put(one_line)
+
+
+with ThreadPoolExecutor() as executor:
+    all_futures = []
+    start_time = time.time()
+    for one_filename in glob.glob('*.py'):
+        one_future = executor.submit(all_lines, one_filename)
+        all_futures.append(one_future)
+
+    done, not_done = wait(all_futures)
+
+
+while not q.empty():
+    print(q.get())
+
+end_time = time.time()
+print(f'Total time = {end_time - start_time}')
+
+# threading: Total time = 0.0012621879577636719
+# serial:  = Total time = 0.0006771087646484375
+# executor:  Total time = 0.0044422149658203125
