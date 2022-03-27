806080f2aad0fe3f1b73cd7620a7504a11520db5
Reuven Lerner <reuven@lerner.co.il>
2022-03-27 10:03:36 +0300

    gitautosync update 70

--- async1.py
+++ async1.py
@@ -4,6 +4,8 @@ import asyncio
 
 
 # function that I want to run in the asyncio "event loop"
+# the function is run indirectly, via the event loop
+# it'll run until (a) it ends or (b) it says await, in which case it goes to sleep (similar to yield)
 async def hello(n):
     print(f'{n} Hello!')
     await asyncio.sleep(1)
