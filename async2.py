f1461688f7a9c40ad05d5d3e52213f86aafed1f9
Reuven Lerner <reuven@lerner.co.il>
2022-03-27 10:01:33 +0300

    gitautosync update 64

---
+++ async2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+
+async def hello(n):
+    print(f'{n} Hello!')
+    await asyncio.sleep(1)
+    print(f'{n} Goodbye!')
+
+
+async def main():
+    for i in range(5):
+        await hello(i)
+
+asyncio.run(main())   # run our async def function
