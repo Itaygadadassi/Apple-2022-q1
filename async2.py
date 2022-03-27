a8036bd636ae69686516ff2ef948eff1f8a29d93
Reuven Lerner <reuven@lerner.co.il>
2022-03-27 10:07:27 +0300

    gitautosync update 84

--- async2.py
+++ async2.py
@@ -15,6 +15,9 @@ async def hello(n):
 # instead of using "await", we'll use "create_task" to add the function
 # to the event loop.
 async def main():
+
+    # add these three tasks to the event loop
+
     t1 = asyncio.create_task(hello(1))
     t2 = asyncio.create_task(hello(2))
     t3 = asyncio.create_task(hello(3))
