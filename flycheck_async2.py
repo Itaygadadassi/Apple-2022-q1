cc4309b1899993ca39e471b49fa64330a22b8694
Reuven Lerner <reuven@lerner.co.il>
2022-03-27 10:05:25 +0300

    gitautosync update 74

---
+++ flycheck_async2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+
+# function that I want to run in the asyncio "event loop"
+# the function is run indirectly, via the event loop
+# it'll run until (a) it ends or (b) it says await, in which case it goes to sleep (similar to yield)
+async def hello(n):
+    print(f'{n} Hello!')
+    await asyncio.sleep(1)
+    print(f'{n} Goodbye!')
+
+
+# instead of using "await", we'll use "create_task" to add the function
+# to the event loop.  
+async def main():
+    tasks = []
+    for i in range(5):
+        tasks.append(asyncio.create_task(hello(i)))
+        
+
+asyncio.run(main())   # run our async def function
