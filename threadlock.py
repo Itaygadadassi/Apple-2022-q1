bcea7eb09166044fc6b7dc5371a890a2c3e42d53
Reuven Lerner <reuven@lerner.co.il>
2022-03-27 09:43:21 +0300

    gitautosync update 27

--- threadlock.py
+++ threadlock.py
@@ -11,7 +11,6 @@ def write_to_log(id_number):
         for i in range(10):
             with open(f'log.txt', 'a') as f:
                 for j in range(5):
-                    time.sleep(0.1)
                     f.write(f'{i} Log {id_number} line {j}\n')
 
 
