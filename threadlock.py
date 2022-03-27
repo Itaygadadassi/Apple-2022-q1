fe42e68f506671d57b0916b936b57eed57b6c30f
Reuven Lerner <reuven@lerner.co.il>
2022-03-27 09:40:09 +0300

    gitautosync update 17

--- threadlock.py
+++ threadlock.py
@@ -8,3 +8,8 @@ def write_to_log(id_number):
         with open(f'log.txt', 'a') as f:
             for j in range(5):
                 f.write('{i} Log {id_number} line {j}\n')
+
+
+for thread_id in range(5):
+    t = threading.Thread(target=write_to_log, args=(thread_id,))
+    t.start()
